1a2f6d0847dca5ddead732b09fe7ecd335b94be5
Akshaykumar S <136469118+akshaykumar059004@users.noreply.github.com>
2025-04-17 16:54:06 +0530

    Update: postfunnding_datewise_dashboard_data.sql

--- sql_queries/postfunnding_datewise_dashboard_data.sql
+++ sql_queries/postfunnding_datewise_dashboard_data.sql
@@ -8,7 +8,8 @@ const query = `
     SELECT 5 UNION ALL
     SELECT 6 UNION ALL
     SELECT 7 UNION ALL
-    SELECT 8
+    SELECT 8 UNION ALL
+    SELECT 9
   ) AS s
   LEFT JOIN forms f ON f.status = s.status AND f.user_id = ? AND f.created_at = ?
   GROUP BY s.status
