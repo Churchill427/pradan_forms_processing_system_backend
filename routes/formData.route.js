4c0587b0b938cd0e123d2d6504147a5989250198
Akshay <akshaykumar059004@gmail.com>
2025-05-07 22:29:17 +0530

    Implementation: Form Submission(without s3) - Farm Pond

--- routes/formData.route.js
+++ routes/formData.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {postLandformData} = require("../controllers/landformData.controller");
+const {postPondformData} = require("../controllers/pondformData.controller");
 
 router.route('/postLandformData').post(postLandformData);
+router.route('/postPondformData').post(postPondformData);
 
 module.exports = router;
